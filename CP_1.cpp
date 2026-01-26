08fad69c3f93bb2bc8d524182d46675f3e3347bc
SH1ROH1 <arkadipetrecko@gmail.com>
2026-01-26 22:42:50 +0200

    26.01.26
    small tests

--- CP_1.cpp
+++ CP_1.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main(void) {
-		
+
+	// додав правильне відображання виводу української
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8); 
+	
+	// Змінні
 	int a = 1;
-	float b = 2;
-	cout << a << endl;
+	float b = 2.3e-2; // лавуча точка
+
+	// вивід у консоль
+	cout << a << " " << b << endl << b*23e1 /*використовую числа з плавучою точкою*/ << endl;
+	std::cout << "Hello World!";
+	cout << "Хей!";
+
+	// Закриття на Ентер
   cin.get();
 }
