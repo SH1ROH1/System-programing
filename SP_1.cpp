b96d81fbdb417047d99f3a3f494106ef5494fc86
SH1ROH1 <107812768+SH1ROH1@users.noreply.github.com>
2026-01-29 18:14:01 +0200

    Created SP_1.cpp

---
+++ SP_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+double z1_2 = 5.12 /* 1.2) */
+
+int main(void) {
+  //1)
+  
+  char x1 = "A";
+  char y1 = "B";
+  char z1 = "C";
+
+  //1.2)
+
+  int x1_2 = 10;
+  
+  //2)
+  
+  int x2 = 56;
+  unsigned long y2 = 65687653;
+  
+  //3)
+
+  int notebooks = 5;
+  int pencils = 3;
+  int rulers = 1;
+
+  double notebookPrice = 12.5;
+  double pencilPrice = 5.0;
+  double rulerPrice = 20.0;
+
+  totalCost = notebooks * notebookPrice + pencils * pencilPrice + rulers * rulerPrice;
+
+}
