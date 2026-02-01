1c76e3de3fb8b7bb0b9dbfafd23aa1e33ca965ab
SH1ROH1_laptop <107812768+SH1ROH1@users.noreply.github.com>
2026-02-01 10:56:11 +0200

    Create SP_2.cpp

---
+++ SP_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void)
+{
+    int a = 10,b = 20,c = 30;
+    printf("a==%d \n b==%d \n c==%d \n ", a, b, c);
+    printf("/%d/\n",336);
+    printf("/%2d/\n",336);
+    printf("/%10d/\n",336);
+    printf("/%-10d/\n",336);
+}
